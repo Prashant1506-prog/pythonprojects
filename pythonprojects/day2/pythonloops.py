3fba1c584f4621d398d5968f313b4464f2f31d26
prashant <prashantlalawat15@gmail.com>
2026-08-08 01:55:13 +0530

    pallindrome code

--- pythonprojects/day2/pythonloops.py
+++ pythonprojects/day2/pythonloops.py
@@ -9,3 +9,18 @@ total = 0
 for i in range(1, n + 1):
     total += i
 print(f"The sum of numbers from 1 to {n} is: {total}")
+
+num = int(input("Enter a number: "))
+
+original = num
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num //= 10
+
+if original == reverse:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
