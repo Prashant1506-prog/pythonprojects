5fa119c44b1bfd860b33abd66265e9b76f8c1a1e
prashant <prashantlalawat15@gmail.com>
2026-08-08 01:58:48 +0530

    star pattern

--- pythonprojects/day2/pythonloops.py
+++ pythonprojects/day2/pythonloops.py
@@ -24,3 +24,9 @@ if original == reverse:
     print("Palindrome")
 else:
     print("Not Palindrome")
+
+for i in range(1, 6):
+    print("*" * i)
+
+for i in range(1, 6):
+    print(" " * (5 - i) + "*" * i)
