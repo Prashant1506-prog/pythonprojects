4606dd4f7e899d74cd8ec90a4a474d637c0a056f
prashant <prashantlalawat15@gmail.com>
2026-08-07 16:42:34 +0530

    the sum of numbers from 1 to n

--- pythonprojects/day2/pythonloops.py
+++ pythonprojects/day2/pythonloops.py
@@ -1,5 +1,11 @@
-"""for i in range(1, 11):
-    print(i)"""
+for i in range(1, 11):
+    print(i)
 
 for v in range(0, 10, 2):
     print(v)
+
+n = int(input("Enter a number: "))
+total = 0
+for i in range(1, n + 1):
+    total += i
+print(f"The sum of numbers from 1 to {n} is: {total}")
