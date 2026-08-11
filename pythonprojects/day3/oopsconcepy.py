c1f9fba2537843cf36678ff1bba5bf1971aa4f56
prashant <prashantlalawat15@gmail.com>
2026-08-11 17:03:53 +0530

    oop simple program

--- pythonprojects/day3/oopsconcepy.py
+++ pythonprojects/day3/oopsconcepy.py
@@ -8,4 +8,28 @@ class BankAccount:
 
     def show_balance(self):
         print(self.balance)
-print("hh")
+
+        
+class Student:
+
+    def __init__(self, name, roll_no, marks):
+        self.name = name
+        self.roll_no = roll_no
+        self.marks = marks
+
+    def display(self):
+        print("Name:", self.name)
+        print("Roll No:", self.roll_no)
+        print("Marks:", self.marks)
+
+    def result(self):
+        if self.marks >= 40:
+            print("Result: Pass")
+        else:
+            print("Result: Fail")
+
+
+student1 = Student("Prashant", 101, 85)
+
+student1.display()
+student1.result()
