e1630f23dc4d7dffab20afcbfe5c32b9386517b4
prashant <prashantlalawat15@gmail.com>
2026-08-11 16:14:07 +0530

    data encapsulation

---
+++ pythonprojects/day3/oopsconcepy.py
@@ -0,0 +1,11 @@
+class BankAccount:
+
+    def __init__(self, balance):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def show_balance(self):
+        print(self.balance)
+print("hh")
