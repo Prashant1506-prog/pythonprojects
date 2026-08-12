631259e15d855d92f3145b60d5a93dc24ebf8a5d
prashant <prashantlalawat15@gmail.com>
2026-08-12 14:46:26 +0530

    multileve inheritance

--- pythonprojects/day4/inheritance.py
+++ pythonprojects/day4/inheritance.py
@@ -35,3 +35,23 @@ dog = Dog()
 
 dog.eat()
 dog.bark()
+
+# multilevel inher
+class Animal:
+    def eat(self):
+        print("Eating")
+
+
+class Mammal(Animal):
+    def walk(self):
+        print("Walking")
+
+
+class Dog(Mammal):
+    def bark(self):
+        print("Barking")
+
+dog = Dog()
+dog.eat()
+dog.walk()
+dog.bark()
