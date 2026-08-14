d8c885437d22bd3875bc34ab477d0270f436f3e8
prashant <prashantlalawat15@gmail.com>
2026-08-14 12:29:35 +0530

    catch all the errors in a single block of except

--- pythonprojects/day5/codes.py
+++ pythonprojects/day5/codes.py
@@ -1,4 +1,4 @@
-"""n = int(input("Enter a number: "))
+n = int(input("Enter a number: "))
 s = int(input("Enter a number: "))
 
 
@@ -9,7 +9,7 @@ except ZeroDivisionError:
 else:
     print(f"Result of {n} / {s} is :{res} ")
 finally:
-    print("Execution completed")"""
+    print("Execution completed")
 
 try:
     res = "10" / 8
