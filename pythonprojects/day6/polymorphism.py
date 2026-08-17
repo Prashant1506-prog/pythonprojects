a8bf55653e7eb98615226d54b919a128eff455f7
prashant <prashantlalawat15@gmail.com>
2026-08-17 15:30:51 +0530

    method overrriding by constructor

--- pythonprojects/day6/polymorphism.py
+++ pythonprojects/day6/polymorphism.py
@@ -52,3 +52,23 @@ class Dog(Animal):
 
 obj = Dog()
 obj.display()
+
+#method overriding with super() and constructor
+
+class Employee:
+    def __init__(self):
+        self.role = "Employee"
+    def display(self):
+        print("Role: ", self.role)
+class Manager(Employee):
+    def __init__(self):
+        super().__init__()
+        self.role = "manager"
+
+    def display(self):
+        print("Role: ", self.role)
+
+e1 = Employee()
+e2 = Manager()
+e1.display()
+e2.display()
