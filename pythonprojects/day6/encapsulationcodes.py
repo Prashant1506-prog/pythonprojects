f5f644c170b153d2cc9c9d7aa424d6691baa0009
prashant <prashantlalawat15@gmail.com>
2026-08-17 11:51:19 +0530

    getter and setter method

---
+++ pythonprojects/day6/encapsulationcodes.py
@@ -0,0 +1,18 @@
+#getter and setter methods
+
+class Employee:
+    def __init__(self):
+        self.__salary = int(input("Enter the current salary:"))
+    def get_salary(self):
+        return self.__salary
+    def set_salary(self, new_salary):
+        if new_salary > 0:
+            self.__salary = new_salary
+        else:
+            print("Invalid salary amount!")
+
+emp = Employee()
+print(f"current salary: {emp.get_salary()}")
+emp.set_salary(int(input("Enter the new salary:")))
+print(f"Updated salary: {emp.get_salary()}")
+        
