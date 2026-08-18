a76a39667d3b600063b834a49f9ef8ee696efa0d
prashant <prashantlalawat15@gmail.com>
2026-08-18 13:01:04 +0530

    decorators with args and kwargs

--- pythonprojects/day7/decorators.py
+++ pythonprojects/day7/decorators.py
@@ -13,3 +13,19 @@ def greet ():
 greet = decorator(greet)
 greet()
 
+#decorators with args and kwargs
+
+def decorator_name(func):
+    def wrapper(*args, **kwargs):
+        print("Before execution")
+        result = func(*args, **kwargs)
+        print("After execution")
+        return result
+    return wrapper
+
+@decorator_name
+def add(a, b):
+    return a + b
+
+print(add(5, 3))
+
